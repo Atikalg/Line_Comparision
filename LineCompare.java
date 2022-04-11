7e6b619ae16697ec40dee3eeea654961c58d0fec
atikalg <who.atik@gmail.com>
2022-04-11 16:40:14 +0530

    Added : Line comparision program_welcome message

---
+++ LineCompare.java
@@ -0,0 +1,12 @@
+package com.bridgelab.Assignment_8;
+/**
+ * @author atik
+ * Line comparision program
+ */
+public class LineCompare {
+
+	    public static void main(String args[])
+	    {
+	        System.out.println("Welcome to Line Comparison Computation Program ");
+	    }
+}
