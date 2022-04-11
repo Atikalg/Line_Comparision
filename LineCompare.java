273cb195e9fa6b33d654b49a646c14ed7669a1d2
atikalg <who.atik@gmail.com>
2022-04-11 19:49:27 +0530

    Added : comparing lines are equal or not

--- LineCompare.java
+++ LineCompare.java
@@ -22,31 +22,38 @@ public class LineCompare {
        	 System.out.println("Enter the value of x11 of line 1 ");
             x11 = sc.nextInt();
             System.out.println("Enter the value of x12 of line 1");
-            int x12 = sc.nextInt();
+             x12 = sc.nextInt();
             System.out.println("Enter the value of y11 of line 1 ");
-            int y11 = sc.nextInt();
+             y11 = sc.nextInt();
             System.out.println("Enter the value of y12 of line 1 ");
-            int y12 = sc.nextInt();
+             y12 = sc.nextInt();
             System.out.println("Enter the value of x21 of line 2");
-            int x21 = sc.nextInt();
+             x21 = sc.nextInt();
             System.out.println("Enter the value of x22 of line 2 ");
-            int x22 = sc.nextInt();
+             x22 = sc.nextInt();
             System.out.println("Enter the value of y21 of line 2 ");
-            int y21 = sc.nextInt();
+             y21 = sc.nextInt();
             System.out.println("Enter the value of y22 of line 2 ");
-            int y22 = sc.nextInt();
+             y22 = sc.nextInt();
        	}
+	 //checking lines are equal or not
+	  public void equals()
+	    {
+	        int m1,m2;
+	        m1 = (y12-y11)/(x12-x11);
+	        m2 = (y22-y21)/(x22-x21);
+	        if(m1 == m2)
+	            System.out.println("Two lines are equal.");
+	        else
+	            System.out.println("Two lines are not equal.");
+	    }
 	    public static void main(String args[])
 	    {
 	        System.out.println("Welcome to Line Comparison Computation Program ");     
 	        LineCompare obj = new LineCompare();
 	        obj.Input();
-	   	//Calculating length of lines
-			 int lengthOfLine1=(int) Math.sqrt((obj.x12-obj.x11) * (obj.x12-obj.x11) + (obj.y12-obj.y11) * (obj.y12-obj.y11));
-		     System.out.println("Length of Line1 is:"+lengthOfLine1);
-	        int lengthOfLine2=(int) Math.sqrt((obj.x22-obj.x21) * (obj.x22-obj.x21) + (obj.y22-obj.y21) * (obj.y22-obj.y21));
-	        System.out.println("Length of Line2:"+lengthOfLine2);
-	        
+	        obj.equals();
+
 	    }
 }
 
